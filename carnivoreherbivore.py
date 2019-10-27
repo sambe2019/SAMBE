06f43798e1052b5e1c1508ef76eb77e6ae817d6c
sambe2019 <57061391+sambe2019@users.noreply.github.com>
2019-10-27 11:21:48 +0100

    Add files via upload

---
+++ carnivoreherbivore.py
@@ -0,0 +1,23 @@
+class carnivore:
+    def __init__(self,p):
+        self._poidsViande = p
+    def devorer(self):
+        print("Je mange ",self._poidsViande,"kilogs de steack par jour")
+
+
+
+class herbivore:
+    def __init__(self,p):
+        self._poidsHerbe = p
+    def brouter(self):
+        print("Je mange ",self._poidsHerbe,"kilogs de gazon par jour")
+
+
+class omnivore(carnivore,herbivore):
+    def __init__(self,pv,ph,h):
+        carnivore.__init__(self,pv)
+        herbivore.__init__(self,ph)
+        self.__humain = h
+teddy = omnivore(10,5,False)
+teddy.devorer()
+teddy.brouter()
